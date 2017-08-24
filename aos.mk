675b4dafb4ee43a816154ef885ede86b27b48f7f
Real man born on semtember <hieunam03@gmail.com>
2017-08-24 12:53:53 +0700

    AOS bringup

--- aos.mk
+++ aos.mk
@@ -1,13 +1,15 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common AtomicOS stuff.
+$(call inherit-product, vendor/aos/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/kyleproxx/device_kyleproxx.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := kyleproxx
-PRODUCT_NAME := lineage_kyleproxx
+PRODUCT_NAME := aos_kyleproxx
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := GT-S758x
 PRODUCT_CHARACTERISTICS := phone
+
+WITH_ROOT := true

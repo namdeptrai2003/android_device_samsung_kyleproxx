a7b67b35d5d3b1272eda33effb00335cd252707d
Real man born on semtember <hieunam03@gmail.com>
2017-08-27 14:03:19 +0700

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -1,12 +1,9 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
 # Inherit device configuration
 $(call inherit-product, device/samsung/kyleproxx/device_kyleproxx.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := kyleproxx
-PRODUCT_NAME := lineage_kyleproxx
+PRODUCT_NAME := abc_kyleproxx
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := GT-S758x

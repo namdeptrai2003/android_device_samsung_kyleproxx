53d86fe095ec74dddae7d54b57b5b667a9a93f09
Real man born on semtember <hieunam03@gmail.com>
2017-08-28 13:39:15 +0700

    Aosp bringup

--- aosp.mk
+++ aosp.mk
@@ -1,12 +1,12 @@
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/kyleproxx/device_kyleproxx.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := kyleproxx
-PRODUCT_NAME := lineage_kyleproxx
+PRODUCT_NAME := aosp_kyleproxx
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := GT-S758x

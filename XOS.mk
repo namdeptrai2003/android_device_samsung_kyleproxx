e79eb1cc98c07c7e9ad5ce3149b2652f0d2b052c
Real man born on semtember <hieunam03@gmail.com>
2017-08-29 12:47:20 +0700

    XOS bringup

---
+++ XOS.mk
@@ -0,0 +1,20 @@
+# Inherit device configuration
+$(call inherit-product, device/samsung/kyleproxx/device_kyleproxx.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/xos/config/common.mk)
+
+PRODUCT_NAME := XOS_kyleproxx
+PRODUCT_DEVICE := oneplus2
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_BRAND := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=Samsung/TrendPlus/kyleproxx:7.1.2/MMB29M/1447841500:user/release-keys \
+    PRIVATE_BUILD_DESC="kyleproxx-user 7.1.2 MMB29M 8 dev-keys"
+

9376ffadde6c798c73e4b0449afbd094ceb7f9e8
Real man born on semtember <hieunam03@gmail.com>
2017-08-31 13:26:22 +0700

    AOSP bringup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,6 +1,11 @@
 # Inherit from the proprietary version
 -include vendor/samsung/kyleproxx/BoardConfigVendor.mk
 
+# Seccomp policy
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy
+ 
+ 
 # Platform
 TARGET_ARCH                                 := arm
 TARGET_NO_BOOTLOADER                        := true
